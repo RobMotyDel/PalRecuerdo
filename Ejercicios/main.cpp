9f8a43b0b3bdc55c48d0ce47df3e6d3e69d3cb99
Rob Monty Del <71721852+RobMotyDel@users.noreply.github.com>
2022-09-09 12:10:56 -0500

    Added files from Documents

---
+++ Ejercicios/main.cpp
@@ -0,0 +1,23 @@
+/* RMD
+ * rob.monty.del@comunidad.unam.mx
+ * 09/09/2022*/
+
+
+
+#include <iostream>
+using namespace std;
+#define N 10
+
+int main() {
+    //definir un arreglo
+    int arr[N]{1};
+    int n = sizeof(arr)/ sizeof(arr[0]);
+    //iterar arreglo
+    for(int i=0;i<N;i++){
+        arr[i]= i+i;
+        cout << arr[i] << "\t";
+    }
+
+
+    return 0;
+}
